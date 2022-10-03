1c45f78167444155bca7f82a61d7a38ac6d6867d
Kiril <kiril.strykalenko@ilcoud.com>
2022-10-03 13:42:08 +0300

    laboratory-3

--- Book/Book/Program.cs
+++ Book/Book/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Book
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Write your book title? ");
+            string title = Console.ReadLine();
+            Console.Write("Write  author name  : ");
+            string author = Console.ReadLine();
+            Console.WriteLine("Write  content : ");
+            string content = Console.ReadLine();
+            Book book = new Book(new Title(title), new Author(author), new Content(content));
+            Console.WriteLine();
+            Console.Clear();
+            book.Title.Show();
+            book.Author.Show();
+            book.Content.Show();
+            Console.WriteLine();
         }
+
+
     }
 }
